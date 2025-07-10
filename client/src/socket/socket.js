fbae94f16841c151ee7f8276ae1d5c2c5f4ce896
prevyne <prevyneoketch6@gmail.com>
2025-07-10 07:12:42 +0300

    front-backend link

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+//const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+const SOCKET_URL = 'https://week-5-web-sockets-assignment-prevyne.onrender.com';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
