82195e7eb0fb8f2cfe76e535ef9cfbee47e04e89
prevyne <prevyneoketch6@gmail.com>
2025-07-12 15:02:40 +0300

    socket url update

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
-//const SOCKET_URL = 'https://week-5-web-sockets-assignment-prevyne.onrender.com';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000' || 'https://week-5-web-sockets-assignment-prevyne.onrender.com';
 
 // The connection is now configured to send the auth token from localStorage
 export const socket = io(SOCKET_URL, {
